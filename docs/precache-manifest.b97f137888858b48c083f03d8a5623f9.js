258e8529e0b73eaa91b56de2d9dc36015b94ec1a
Dmitriy Putilov <dmitrii.98.putilov@gmail.com>
2020-03-28 08:13:46 +0500

    add docs for github.io

---
+++ docs/precache-manifest.b97f137888858b48c083f03d8a5623f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb982765a15ec4335d9329ea771353dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf72c53ed2411fb09db2",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "2135433f551e91fa2744",
+    "url": "/static/js/2.1f18736f.chunk.js"
+  },
+  {
+    "revision": "bf72c53ed2411fb09db2",
+    "url": "/static/js/main.ec0e8660.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
